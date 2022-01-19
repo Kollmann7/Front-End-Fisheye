bb64c285d37788310980ec3a9ea2253028526c60
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-01-19 12:51:19 +0100

    photographers cards

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,29 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, id, city, country, tagline, price } = data;
+    
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const $wrapper = document.createElement("div");
+    $wrapper.classList.add("photographer_card");
+    $wrapper.classList.add(id);
+            
+    const photograph = `
+    <a href="./photographer.html?id=${id}">
+      <div class="photographer_img">
+        <img src="${picture}" alt="">
+        <h2>${name}</h2>
+      </div>
+    </a>
+    <div class="photographer_info">
+      <p class="city">${city + ", " + country}</p>
+      <p class="tagline">${tagline}</p>
+      <p class="price">${price + "&euro;" + "/jour"}</p>
+    </div>
+    `;
+    $wrapper.innerHTML = photograph;
+
+    return $wrapper;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, city, country, tagline, price, getUserCardDOM }
 }
