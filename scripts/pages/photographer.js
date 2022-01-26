76f4e9a29d3190308df575f08a93239796ab5043
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-01-26 16:18:13 +0100

    add a function to fetch profils

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,31 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const profilSection = document.querySelector(".photograph_header");
+
+async function getProfilMedia() {
+  try {
+    let response = await fetch('../../data/photographers.json')
+    let result = await response.json()
+    return result
+  }  
+  catch (error) {
+    console.error('mon fetch a pété', e)
+  }
+  }
+  
+
+async function displayProfilMedia( profils, media ) {
+  
+  const profil = profils.find(profil => profil.id === parseInt(id));
+  console.log(profil)
+  const profilModel = profilFactory(profil);
+  const profilDOM = profilModel.getProfilCardDOM();
+  profilSection.appendChild(profilDOM);
+
+}
+async function init() {
+    const {media} = await getProfilMedia();
+    const {photographers} = await getProfilMedia();
+    displayProfilMedia(photographers, media);
+  }
+init();
