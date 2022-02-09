008130d40325715fb4164af5149f34f664978744
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-02-09 13:40:09 +0100

    upgrade displayProfilMedia

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 const profilSection = document.querySelector(".photograph_header");
+const mediaSection = document.querySelector(".photographers_media");
 
 async function getProfilMedia() {
   try {
@@ -11,21 +12,27 @@ async function getProfilMedia() {
   catch (error) {
     console.error('mon fetch a pété', e)
   }
-  }
+}
   
 
-async function displayProfilMedia( profils, media ) {
+async function displayProfilMedia( profils, medias ) {
   
   const profil = profils.find(profil => profil.id === parseInt(id));
   console.log(profil)
+  const mediasFromPhotographer = medias.filter(media => media.photographerId === parseInt(id));
   const profilModel = profilFactory(profil);
   const profilDOM = profilModel.getProfilCardDOM();
   profilSection.appendChild(profilDOM);
-
+  mediasFromPhotographer.forEach((media) => {
+    const mediaModel = mediaFactory(media);
+    const mediaDOM = mediaModel.getMediaCardDOM();
+    mediaSection.appendChild(mediaDOM);
+});
+  
 }
 async function init() {
-    const {media} = await getProfilMedia();
-    const {photographers} = await getProfilMedia();
-    displayProfilMedia(photographers, media);
-  }
+  const {media} = await getProfilMedia();
+  const {photographers} = await getProfilMedia();
+  displayProfilMedia(photographers, media);
+}
 init();
